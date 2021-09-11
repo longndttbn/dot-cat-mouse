ce2941e8b6300eb37fe02fe25a25cfaf4f5a257d
Kane <longnd@ikame.vn>
2021-09-11 11:09:57 +0700

    Commit Cat class

--- Cat.js
+++ Cat.js
@@ -3,4 +3,9 @@ function Cat() {
 	this.stomach = [];
 }
 
+Cat.prototype.eat = function(mouse) {
+	// body...
+	this.stomach.push(mouse);
+};
+
 module.exports= Cat;
--- Mouse.js
+++ Mouse.js
@@ -1,3 +1,12 @@
-function run(argument) {
+function Mouse(name) {
 	// body...
+	this.name = name;
+	this.die = false;
 }
+
+Mouse.prototype.die = function(die) {
+	// body...
+	this.die = true;
+};
+
+module.exports = Mouse;
