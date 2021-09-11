6b7c5cd2585efee33b891dd22b365dcb79d8063b
Kane <longnd@ikame.vn>
2021-09-11 16:03:03 +0700

    Add all chalk

--- Dog.js
+++ Dog.js
@@ -1,11 +1,18 @@
-function Dog(argument) {
+var chalkObject = require('chalk');
+function Dog(name) {
 	// body...
 	this.stomach = [];
+	this.name = name;
 }
 
 Dog.prototype.eat = function(cat) {
 	// body...
 	this.stomach.push(cat);
+};
+
+Dog.prototype.sayHello = function() {
+	//console.log('iok')
+	console.log('Hello Long '+ chalkObject.red(this.name));
 };
 
 module.exports = Dog;
